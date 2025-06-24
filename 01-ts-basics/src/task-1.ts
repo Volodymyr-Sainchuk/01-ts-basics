48216ee2d3e74f38e78aaae21831bd202abbac4c
Volodymyr <sainchukvova2104@gmail.com>
2025-06-24 19:26:05 +0300

    do hw

---
+++ 01-ts-basics/src/task-1.ts
@@ -0,0 +1,11 @@
+type Name = string;
+type Age = number;
+type Online = boolean;
+
+const name: Name = "Alice";
+const age: Age = 30;
+const isOnline: Online = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
